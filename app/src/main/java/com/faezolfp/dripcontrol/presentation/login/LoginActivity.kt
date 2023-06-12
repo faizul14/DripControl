debce6ff827b763563710a7db159b58bbbbe6d59
Faezol <fpcyber61@gmail.com>
2023-06-12 16:55:16 +0800

    + Add layout Register, Login, SplashScreen

---
+++ app/src/main/java/com/faezolfp/dripcontrol/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.faezolfp.dripcontrol.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.faezolfp.dripcontrol.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/faezolfp/dripcontrol/presentation/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.faezolfp.dripcontrol.presentation.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.faezolfp.dripcontrol.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
