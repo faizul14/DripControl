7db9205123a0c1395faa4aba2349fccbc68d7d0f
Faezol <fpcyber61@gmail.com>
2023-07-09 21:20:18 +0800

    + Add Layout

---
+++ app/src/main/java/com/faezolfp/dripcontrol/presentation/addpasien/InsertPasienActivity.kt
@@ -0,0 +1,12 @@
+package com.faezolfp.dripcontrol.presentation.addpasien
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.faezolfp.dripcontrol.R
+
+class InsertPasienActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert_pasien)
+    }
+}
---
+++ app/src/main/java/com/faezolfp/dripcontrol/presentation/listpasien/ListPasienActivity.kt
@@ -0,0 +1,12 @@
+package com.faezolfp.dripcontrol.presentation.listpasien
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.faezolfp.dripcontrol.R
+
+class ListPasienActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_pasien)
+    }
+}
